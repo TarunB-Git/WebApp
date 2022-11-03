88848d60bfa92ee3291416d44cec134337b1be4a
Nilores <studentk1000@gmail.com>
2022-11-03 06:23:01 +0000

    Added Mysql

--- a.py
+++ a.py
@@ -1,7 +1,21 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="STUDENTK",
+    password="bytew0rld",
+    hostname="STUDENTK.mysql.pythonanywhere-services.com",
+    databasename="STUDENTK$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
